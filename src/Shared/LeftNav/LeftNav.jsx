959b28240f36c6c458e7d86decf104078b0a6ed6
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2023-04-21 01:50:12 +0600

    left side work done

--- src/Shared/LeftNav/LeftNav.jsx
+++ src/Shared/LeftNav/LeftNav.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import LeftCategory from '../Leftcategory/LeftCategory';
 
 const LeftNav = () => {
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() =>{
+        fetch('http://localhost:5000/categories')
+        .then(res => res.json())
+        .then(data => setCategories(data))
+        .catch(error => console.log(error))
+    }, [])
     return (
-        <div>
-            <h3>Left Nav</h3>
+        <div className='pt-5 '>
+            <h3 className='mb-3'>All Category</h3>
+            {
+                categories.map(category => <LeftCategory
+                key={category.id}
+                category={category}
+                ></LeftCategory>)
+            }
         </div>
     );
 };
---
+++ src/Shared/Leftcategory/LeftCategory.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './LeftCategory.css'
+
+const LeftCategory = ({category}) => {
+
+    const {name} = category;
+
+    return (
+        <div className='ps-4 mb-3'>
+            <Link to={`/category/${category.id}`} className='text-decoration-none text-black fw-bold fs-5 '>{name}</Link>
+        </div>
+    );
+};
+
+export default LeftCategory;
