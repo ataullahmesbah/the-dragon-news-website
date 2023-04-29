5056cb6e7b9f2081a18655f1f5fdd369167a82e2
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2023-04-30 02:17:02 +0600

    rating issue

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -44,7 +44,7 @@ const router = createBrowserRouter([
             {
                 path: ':id',
                 element: <Category></Category>,
-                loader: ({params}) => fetch(`http://localhost:5000/categories/${params.id}`)
+                loader: ({params}) => fetch(`https://the-news-dragon-server-ataullahmesbah.vercel.app/categories/${params.id}`)
             }
         ]
     },
@@ -55,7 +55,7 @@ const router = createBrowserRouter([
             {
                 path: ':id',
                 element: <PrivateRoute><News></News></PrivateRoute>,
-                loader: ({params}) => fetch(`http://localhost:5000/news/${params.id}`)
+                loader: ({params}) => fetch(`https://the-news-dragon-server-ataullahmesbah.vercel.app/news/${params.id}`)
             }
         ]
     }
